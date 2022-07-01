ffd20d481a6dac761e994511409e9b5f37df0b61
seaho5599 <seaho5599@gmail.com>
2022-07-01 17:26:06 +0900

    1차코딩시작,visual스와이퍼,메뉴js작업

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+});
+window.onload = function () {
+  // 메뉴 기능
+  let header = $('.header');
+  let gnb = $('.gnb');
+  let gnbH = gnb.height();
+
+  gnb.mouseenter(function(){
+    header.css('height', gnbH);
+  });
+  gnb.mouseleave(function(){
+    header.css('height', 70);
+  });
+
+  //비주얼 슬라이드
+  new Swiper('.sw-visual', {
+    slidesPerView: 8,
+    loop: true,
+    navigation: {
+      nextEl: ".sw-visual-next",
+      prevEl: ".sw-visual-prev",
+    },
+  });
+  
+};
